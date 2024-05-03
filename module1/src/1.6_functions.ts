dd8797bf46f608c9f872f343cf7647c1753736fb
Dipra <dipradas5940@gmail.com>
2024-05-03 20:26:57 +0600

    [1.7] Spread and Rest Operator

--- module1/src/1.6_functions.ts
+++ module1/src/1.6_functions.ts
@@ -1,17 +1,27 @@
-// Normal function
-// Arrow function
+{
+    // Normal function
+    // Arrow function
 
-function add(num1: number, num2: number = 10): number {
-    return num1 + num2;
+    function add(num1: number, num2: number = 10): number {
+        return num1 + num2;
+    }
+
+    add(2, 3);
+
+
+    const addArrow = (num1: number, num2: number): number => num1 + num2;
+
+    const poorUser = {
+        name: 'Dipra',
+        balance: 0,
+        addBalance(balance: number): string {
+            return `My new balance is : ${this.balance + balance}`;
+        }
+    }
+
+    // objject --> function --> method
+
+    const arr: number[] = [1, 3, 4];
+
+    const newArray: number[] = arr.map((elem: number): number => elem * elem);
 }
-
-add(2, 3);
-
-
-const addArrow = (num1: number, num2: number): number => num1 + num2;
-
-// objject --> function --> method
-
-const arr: number[] = [1, 3, 4];
-
-const newArray: number[] = arr.map((elem: number): number => elem * elem);
