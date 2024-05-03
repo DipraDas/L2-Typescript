ce5ad201d3b2c7c072fafe4d480f494dcf0d5a46
Dipra <dipradas5940@gmail.com>
2024-05-03 12:56:57 +0600

    [1.4] Basic Data Type

---
+++ module1/src/datatype.ts
@@ -0,0 +1,31 @@
+// Basic Data Type
+
+//string
+let firstName: string = 'Dipra'
+
+//number
+let roll: number = 123;
+
+//boolean
+let isAdmin: boolean = true;
+
+//undefined
+let x: undefined = undefined;
+
+//null
+let y: null = null;
+
+let d: number;
+d = 123;
+
+//Array
+let friends: string[] = ['rahel', 'monica']
+
+let elegibleRollList: number[] = [1, 3];
+elegibleRollList.push(5);
+
+let ageName: [number, string, boolean] = [50, 'Mr.X', true];
+
+ageName[0] = 68;
+
+
