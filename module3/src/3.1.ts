35854b63e6a86b430c57685c6d8abf9e8784064d
Dipra <dipradas5940@gmail.com>
2024-05-05 20:46:46 +0600

    [3.1] Class and Object

---
+++ module3/src/3.1.ts
@@ -0,0 +1,21 @@
+class Animal {
+    // public name: string;
+    // public species: string;
+    // public sound: string;
+
+
+    // ! parameter properties
+    constructor(public name: string, public species: string, public sound: string) {
+        // this.name = name;
+        // this.species = species;
+        // this.sound = sound;
+    }
+
+    makeSound() {
+        console.log(`This ${this.name} says ${this.sound}`);
+    }
+}
+
+
+const dog = new Animal('German Shepard', 'dog', 'Ghew ghew')
+const cat = new Animal('Persian Vai', 'cat', 'Mew Mew')
