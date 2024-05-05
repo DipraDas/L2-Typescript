ff23721e40cc9c3abbe718f19ddd4e7542b90760
Dipra <dipradas5940@gmail.com>
2024-05-05 21:11:08 +0600

    [3.2] Inheritance in OOP

--- module2/src/2.5.ts
+++ module2/src/2.5.ts
@@ -1,40 +1,41 @@
-// Function with generic
+{// Function with generic
 
-const createArray = (param: string): string[] => {
-    return [param]
-}
-
-const createArrayWithGeneric = <T>(param: T): T[] => {
-    return [param];
-}
-
-const res1 = createArray('Bangladesh');
-const resGeneric = createArrayWithGeneric<string>('Bangladesh');
-
-type User = { id: number; name: string };
-
-const resGenericObj = createArrayWithGeneric<User>({
-    id: 2222,
-    name: 'MR.X'
-})
-
-const createArrayWithTuple = <T, Q>(param1: T, param2: Q): [T, Q] => {
-    return [param1, param2]
-}
-
-const res10 = createArrayWithTuple<string, number>('Bangladesh', 222);
-
-const res11 = createArrayWithTuple<string, { name: string }>(
-    'Bangladesh', { name: 'Asia' }
-)
-
-const addCourseToStudent = <T>(student: T) => {
-    const course = 'Next Lvl Web Development';
-    return {
-        ...student,
-        course
+    const createArray = (param: string): string[] => {
+        return [param]
     }
-}
 
-const student1 = addCourseToStudent({ name: 'Mr.X', email: 'x@gmail.com' });
-const student2 = addCourseToStudent({ name: 'Mr.Y', email: 'Y@gmail.com' });
+    const createArrayWithGeneric = <T>(param: T): T[] => {
+        return [param];
+    }
+
+    const res1 = createArray('Bangladesh');
+    const resGeneric = createArrayWithGeneric<string>('Bangladesh');
+
+    type User = { id: number; name: string };
+
+    const resGenericObj = createArrayWithGeneric<User>({
+        id: 2222,
+        name: 'MR.X'
+    })
+
+    const createArrayWithTuple = <T, Q>(param1: T, param2: Q): [T, Q] => {
+        return [param1, param2]
+    }
+
+    const res10 = createArrayWithTuple<string, number>('Bangladesh', 222);
+
+    const res11 = createArrayWithTuple<string, { name: string }>(
+        'Bangladesh', { name: 'Asia' }
+    )
+
+    const addCourseToStudent = <T>(student: T) => {
+        const course = 'Next Lvl Web Development';
+        return {
+            ...student,
+            course
+        }
+    }
+
+    const student1 = addCourseToStudent({ name: 'Mr.X', email: 'x@gmail.com' });
+    const student2 = addCourseToStudent({ name: 'Mr.Y', email: 'Y@gmail.com' });
+}
--- module3/src/3.1.ts
+++ module3/src/3.1.ts
@@ -1,21 +1,25 @@
-class Animal {
-    // public name: string;
-    // public species: string;
-    // public sound: string;
+{
+
+    class Animal {
+        // public name: string;
+        // public species: string;
+        // public sound: string;
 
 
-    // ! parameter properties
-    constructor(public name: string, public species: string, public sound: string) {
-        // this.name = name;
-        // this.species = species;
-        // this.sound = sound;
+        // ! parameter properties
+        constructor(public name: string, public species: string, public sound: string) {
+            // this.name = name;
+            // this.species = species;
+            // this.sound = sound;
+        }
+
+        makeSound() {
+            console.log(`This ${this.name} says ${this.sound}`);
+        }
     }
 
-    makeSound() {
-        console.log(`This ${this.name} says ${this.sound}`);
-    }
+    const dog = new Animal('German Shepard', 'dog', 'Ghew ghew')
+    const cat = new Animal('Persian Vai', 'cat', 'Mew Mew')
+
+
 }
-
-
-const dog = new Animal('German Shepard', 'dog', 'Ghew ghew')
-const cat = new Animal('Persian Vai', 'cat', 'Mew Mew')
